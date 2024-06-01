9243f45f61ec8aa3d2b9eb4a13ff56d7f6486fea
ankurksingh1312 <ankurksingh1312@gmail.com>
2024-06-01 16:57:39 +0530

    api added in project

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello from Vercel with Python and Poetry!')
+
+def main(req, res):
+    return handler(req, res)

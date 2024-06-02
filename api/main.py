20fd298456817cef80f0baeafeaee12741c216bf
ankurksingh1312 <ankurksingh1312@gmail.com>
2024-06-02 09:27:31 +0530

    html server sider render added

--- api/main.py
+++ api/main.py
@@ -1,11 +1,16 @@
 from http.server import BaseHTTPRequestHandler
+import os
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Hello from Vercel with Python and Poetry!')
+        current_dir = os.path.dirname(__file__)
+        html_file_path = os.path.join(current_dir, 'html', 'index.html')
+        with open(html_file_path, 'rb') as file:
+            html_content = file.read()
+        self.wfile.write(html_content)
 
 def main(req, res):
     return handler(req, res)
